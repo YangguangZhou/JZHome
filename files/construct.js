a25d959f8f2e1b8c7029e2c6f77301f7c7cf426a
周洋光 <yanguangzhousunny@163.com>
2021-12-25 20:47:44 +0800

    first commit

---
+++ files/construct.js
@@ -0,0 +1,1 @@
+(async () => { await loadCSS("/css/reset.css"); await loadCSS("/css/main.css"); await loadCSS("/css/cursor.css"); await loadJS("https://cdn.bootcdn.net/ajax/libs/vue/2.6.12/vue.min.js"); await loadJS("https://cdn.bootcdn.net/ajax/libs/vue-router/3.4.8/vue-router.min.js"); await loadJS("/js/cursor.js"); await loadJS("/js/script.js"); })();
