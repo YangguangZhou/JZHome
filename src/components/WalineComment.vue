2b3a1926d1b844564cb9bd509a34fdbff66286d9
Jerry Zhou <2098412009@qq.com>
2023-03-24 20:30:10 +0800

    Update WalineComment.vue

--- src/components/WalineComment.vue
+++ src/components/WalineComment.vue
@@ -2,12 +2,16 @@
     <div></div>
 </template>
 
+<script type="module">
+    import { init } from 'https://unpkg.com/@waline/client@v2/dist/waline.mjs';
+
+    init({
+      el: '#waline',
+      serverURL: 'https://waline.jerryz.com.cn',
+    });
+</script>
+
 <script>
-import { init } from 'https://unpkg.com/@waline/client@v2/dist/waline.mjs';
-init({
-    el: '#waline',
-    serverURL: 'https://waline.jerryz.com.cn',
-});
 export default {
     name: "WalineComment",
 };
