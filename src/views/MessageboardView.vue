5638bdb7094020a9080fae1651a97abf13e939c9
Jerry Zhou <61733195+YangguangZhou@users.noreply.github.com>
2023-04-16 15:14:25 +0800

    Update MessageboardView.vue

--- src/views/MessageboardView.vue
+++ src/views/MessageboardView.vue
@@ -3,8 +3,8 @@
         <div class="text-4xl sm:text-5xl tracking-wider mb-3 h-14">留言板</div>
         <div class="messageboard">
             <p>你可以在这里留言</p>
-            <Waline :serverURL="serverURL" :path="path" :dark="dark"/>
             <p>你可以点击<LinkTo url="/"><ruby><rb>这里</rb><rt>/index</rt></ruby></LinkTo>返回主页。</p>
+            <Waline :serverURL="serverURL" :path="path" :dark="dark"/>
         </div>
     </span>
 </template>
