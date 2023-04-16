ef26330fa8f2ac6535861039a2b66f8e41f94244
Jerry Zhou <61733195+YangguangZhou@users.noreply.github.com>
2023-04-16 15:17:01 +0800

    Update SponsorView.vue

--- src/views/SponsorView.vue
+++ src/views/SponsorView.vue
@@ -5,9 +5,10 @@
             <p class="font-bold">谢谢你，陌生人。</p>
             <!--p class="qrcode"><img src="/qrcode.png"/></p-->
             <p class="qrcode"> <iframe src="https://pay.jerryz.com.cn/" border="0" frameborder="no" framespacing="0" allowfullscreen="true" height="654" width="100%"></iframe></p>
-            <p>注：建议在支付备注栏填写你的昵称和寄语。</p>
+            <p>注：你可以在下方留下你的昵称和寄语。</p>
             <p>十分感谢为我提供了赞助的伙伴们！你可以<LinkTo url="/sponsorship"><ruby><rb>点击这里</rb><rt>/sponsorship</rt></ruby></LinkTo>查看明细。</p>
             <p>你可以点击<LinkTo url="/"><ruby><rb>这里</rb><rt>/index</rt></ruby></LinkTo>返回主页。</p>
+            <Waline :serverURL="serverURL" :path="path" :dark="dark"/>
         </div>
     </span>
 </template>
@@ -19,6 +20,15 @@ export default {
     name: "SponsorView",
     components: { LinkTo },
 };
+</script>
+
+<script setup>
+import { Waline } from '@waline/client/component';
+import { computed } from 'vue';
+import { useRoute } from 'vue-router';
+const serverURL = 'https://waline.vercel.app';
+const path = computed(() => useRoute().path);
+const dark = true;
 </script>
 
 <style>
