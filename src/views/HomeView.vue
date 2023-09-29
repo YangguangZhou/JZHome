aa26db271053b183c4814b830bc937a4696f16b9
Jerry Zhou <61733195+YangguangZhou@users.noreply.github.com>
2023-09-29 17:55:19 +0800

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,7 +4,7 @@
         <div class="home">
             <p class="font-bold">我叫 Jerry Zhou，</p>
             <p><ruby><rb>“Jerry Zhou”</rb><rt>Used Since 2020</rt></ruby> 是我广泛使用的网名，你可以在<LinkTo url="https://blog.jerryz.com.cn/article/about/"><ruby><rb>这里</rb><rt>/about</rt></ruby></LinkTo>了解到我的一些信息。</p>
-            <p>我现在是一名初三学生，就读于<LinkTo url="https://ynsdfz.net/"><ruby><rb>云南师大附中</rb><rt>YNSDFZ</rt></ruby></LinkTo>。同时我也是一名<ruby><rb>信竞选手</rb><rt>OIer</rt></ruby>，在学习信息学竞赛。我的所有账号都在<LinkTo url="/account" class=""><ruby><rb>这里</rb><rt>/account</rt></ruby></LinkTo>，欢迎你来这些地方找我。</p>
+            <p>我现在是一名高一学生，就读于<LinkTo url="https://ynsdfz.net/"><ruby><rb>云南师大附中</rb><rt>YNSDFZ</rt></ruby></LinkTo>。同时我也是一名<ruby><rb>信竞选手</rb><rt>OIer</rt></ruby>，在学习信息学竞赛。我的所有账号都在<LinkTo url="/account" class=""><ruby><rb>这里</rb><rt>/account</rt></ruby></LinkTo>，欢迎你来这些地方找我。</p>
             <p>我会在<LinkTo url="https://blog.jerryz.com.cn/" mode="stay"><ruby><rb>博客</rb><rt>/blog</rt></ruby></LinkTo>上记录我感兴趣的内容。有时，我会开发一些好玩的<LinkTo url="/project"><ruby><rb>项目</rb><rt>/project</rt></ruby></LinkTo>，它们会在我的<LinkTo url="https://github.com/YangguangZhou"><ruby><rb>GitHub</rb><rt>YangguangZhou</rt></ruby></LinkTo>上。一行行的代码，见证了我的<LinkTo url="/timeline"><ruby><rb>成长</rb><rt>/timeline</rt></ruby></LinkTo>。</p>
             <p>网站的维护需要一定的成本，希望你可以<LinkTo url="/sponsor"><ruby><rb>支持</rb><rt>/sponsor</rt></ruby></LinkTo>我的开发。如果你有什么想对我说的，你可以在<LinkTo url="/messageboard"><ruby><rb>这里</rb><rt>/messageboard</rt></ruby></LinkTo>留言。</p>
             <img src="https://card.jerryz.com.cn/white">
