bcc61af0befb85c4e7f7c6150fa5028ddda9b8af
Jerry Zhou <61733195+YangguangZhou@users.noreply.github.com>
2024-01-19 17:17:19 +0800

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,12 @@
 <template>
     <span>
-        <div class="text-5xl tracking-wider mb-3 h-14">你好。</div>
+        <div class="text-5xl tracking-wider mb-3 h-14">欢迎！</div>
         <div class="home">
-            <p class="font-bold">我叫 Jerry Zhou，</p>
-            <p><ruby><rb>“Jerry Zhou”</rb><rt>Used Since 2020</rt></ruby> 是我广泛使用的网名，你可以在<LinkTo url="https://blog.jerryz.com.cn/article/about/"><ruby><rb>这里</rb><rt>/about</rt></ruby></LinkTo>了解到我的一些信息。</p>
-            <p>我现在是一名高一学生，就读于<LinkTo url="https://ynsdfz.net/"><ruby><rb>云南师大附中</rb><rt>YNSDFZ</rt></ruby></LinkTo>。同时我也是一名<ruby><rb>信竞选手</rb><rt>OIer</rt></ruby>，在学习信息学竞赛。我的所有账号都在<LinkTo url="/account" class=""><ruby><rb>这里</rb><rt>/account</rt></ruby></LinkTo>，欢迎你来这些地方找我。</p>
-            <p>我会在<LinkTo url="https://blog.jerryz.com.cn/" mode="stay"><ruby><rb>博客</rb><rt>/blog</rt></ruby></LinkTo>上记录我感兴趣的内容。有时，我会开发一些好玩的<LinkTo url="/project"><ruby><rb>项目</rb><rt>/project</rt></ruby></LinkTo>，它们会在我的<LinkTo url="https://github.com/YangguangZhou"><ruby><rb>GitHub</rb><rt>YangguangZhou</rt></ruby></LinkTo>上。一行行的代码，见证了我的<LinkTo url="/timeline"><ruby><rb>成长</rb><rt>/timeline</rt></ruby></LinkTo>。</p>
-            <p>网站的维护需要一定的成本，希望你可以<LinkTo url="/sponsor"><ruby><rb>支持</rb><rt>/sponsor</rt></ruby></LinkTo>我的开发。如果你有什么想对我说的，你可以在<LinkTo url="/messageboard"><ruby><rb>这里</rb><rt>/messageboard</rt></ruby></LinkTo>留言。</p>
+            <p class="font-bold">我是 Jerry Zhou，</p>
+            <p>自2020年起，<ruby><rb>“Jerry Zhou”</rb><rt>Used Since 2020</rt></ruby>便是我在网络世界的身份标识。想了解更多关于我？请点击<LinkTo url="https://blog.jerryz.com.cn/article/about/"><ruby><rb>这里</rb><rt>/about</rt></ruby></LinkTo>。</p>
+            <p>作为一名高一的学生，我正在<LinkTo url="https://ynsdfz.net/"><ruby><rb>云南师大附中</rb><rt>YNSDFZ</rt></ruby></LinkTo>努力学习。除此之外，我还是一名<ruby><rb>信息学竞赛选手</rb><rt>OIer</rt></ruby>，同时对生物学抱有浓厚的兴趣。欢迎访问<LinkTo url="/account"><ruby><rb>我的社交账号</rb><rt>/account</rt></ruby></LinkTo>，与我交流互动。</p>
+            <p>我经常在<LinkTo url="https://blog.jerryz.com.cn/"><ruby><rb>博客</rb><rt>/blog</rt></ruby></LinkTo>分享我感兴趣的话题。同时，我也乐于开发<LinkTo url="https://tools.jerryz.com.cn/"><ruby><rb>网站和各种有趣的项目</rb><rt>Tools & Projects</rt></ruby></LinkTo>，这些都可以在我的<LinkTo url="https://github.com/YangguangZhou"><ruby><rb>GitHub</rb><rt>YangguangZhou</rt></ruby></LinkTo>上找到。每一行代码都记录着我成长的足迹，欢迎探索我的<LinkTo url="/timeline"><ruby><rb>成长历程</rb><rt>/timeline</rt></ruby></LinkTo>。</p>
+            <p>维护网站需要花费不少心血和资源，如果你喜欢我的作品，不妨考虑<LinkTo url="/sponsor"><ruby><rb>支持</rb><rt>/sponsor</rt></ruby></LinkTo>我。有什么想对我说的？请在<LinkTo url="/messageboard"><ruby><rb>留言板</rb><rt>/messageboard</rt></ruby></LinkTo>分享你的想法。</p>
             <img src="https://card.jerryz.com.cn/white">
         </div>
     </span>
