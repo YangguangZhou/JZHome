1750ebf2269e3826ffcdacba8f58eeb13a46739c
Jerry Zhou <61733195+YangguangZhou@users.noreply.github.com>
2024-01-29 12:00:13 +0800

    Update SponsorView.vue

--- src/views/SponsorView.vue
+++ src/views/SponsorView.vue
@@ -4,7 +4,7 @@
         <div class="sponsor">
             <p class="font-bold">谢谢你，陌生人。</p>
             <!--p class="qrcode"><img src="/qrcode.png"/></p-->
-            <p class="qrcode"> <iframe src="https://pay.jerryz.com.cn/sponsor" border="0" frameborder="no" framespacing="0" allowfullscreen="true" height="654" width="100%"></iframe></p>
+            <p class="qrcode"> <iframe src="https://pay.jerryz.com.cn/sponsor" border="0" frameborder="no" framespacing="0" allowfullscreen="true" height="668" width="100%"></iframe></p>
             <p>注：你可以在下方留下你的昵称和寄语。</p>
             <p>十分感谢为我提供了赞助的伙伴们！你可以<LinkTo url="/sponsorship"><ruby><rb>点击这里</rb><rt>/sponsorship</rt></ruby></LinkTo>查看明细。</p>
             <p>你可以点击<LinkTo url="/"><ruby><rb>这里</rb><rt>/index</rt></ruby></LinkTo>返回主页。</p>
