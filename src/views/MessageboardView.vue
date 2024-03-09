1f83519bc9d0820f4e773a8e598754bdd26357ff
Jerry Zhou <61733195+YangguangZhou@users.noreply.github.com>
2024-03-09 19:40:30 +0800

    Update MessageboardView.vue

--- src/views/MessageboardView.vue
+++ src/views/MessageboardView.vue
@@ -1,36 +1,58 @@
 <template>
-    <span>
-        <div class="text-4xl sm:text-5xl tracking-wider mb-3 h-14">留言板</div>
-        <div class="messageboard">
-            <p>你可以在这里留言</p>
-            <p>你可以点击<LinkTo url="/"><ruby><rb>这里</rb><rt>/index</rt></ruby></LinkTo>返回主页。</p>
-            <Waline :serverURL="serverURL" :path="path" :dark="dark" :lang="lang"/>
-        </div>
-    </span>
+  <span>
+    <div class="text-4xl sm:text-5xl tracking-wider mb-3 h-14">留言板</div>
+    <div class="messageboard">
+      <div class="content">
+        <p>你可以在这里留言</p>
+        <p>
+          你可以点击<LinkTo url="/"
+            ><ruby><rb>这里</rb><rt>/index</rt></ruby></LinkTo
+          >返回主页。
+        </p>
+      </div>
+      <Waline
+        :serverURL="serverURL"
+        :path="path"
+        :dark="dark"
+        :lang="lang"
+        :emoji="emoji"
+      />
+    </div>
+  </span>
 </template>
 
 <script>
-import LinkTo from '@/components/LinkTo.vue';
+import LinkTo from "@/components/LinkTo.vue";
 
 export default {
-    name: "MessageboardView",
-    components: { LinkTo },
+  name: "MessageboardView",
+  components: { LinkTo },
 };
 </script>
 
 <script setup>
-import { Waline } from '@waline/client/component';
-import { computed } from 'vue';
-import { useRoute } from 'vue-router';
+import { Waline } from "@waline/client/component";
+import { computed } from "vue";
+import { useRoute } from "vue-router";
 
-const serverURL = 'https://waline.jerryz.com.cn';
+const serverURL = "https://waline.jerryz.com.cn";
 const path = computed(() => useRoute().path);
-const dark = 'auto';
-const lang = 'zh';
+const dark = "auto";
+const lang = "zh";
+const emoji = [
+  "//unpkg.com/@waline/emojis@1.2.0/bmoji",
+  "//unpkg.com/@waline/emojis@1.2.0/tw-emoji",
+  "//unpkg.com/@waline/emojis@1.2.0/qq",
+  "//unpkg.com/@waline/emojis@1.2.0/weibo",
+  "//unpkg.com/@waline/emojis@1.2.0/tieba",
+  "//unpkg.com/@waline/emojis@1.2.0/bilibili",
+  "//unpkg.com/@waline/emojis@1.2.0/alus",
+  "//unpkg.com/@waline/emojis@1.2.0/soul-emoji",
+];
 </script>
 
 <style>
-.messageboard p {
-    @apply ind text-base sm:text-lg leading-9 mb-2
+.content {
+  @apply ind text-base sm:text-lg leading-9 mb-2;
 }
 </style>
